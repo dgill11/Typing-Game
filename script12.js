3bd5f3deb9de44043bb1cfe4becf81549b0b8062
Deepinder Gill <86131871+dgill11@users.noreply.github.com>
2021-08-24 15:54:45 -0400

    Update script12.js

--- script12.js
+++ script12.js
@@ -1,21 +1,46 @@
 const inputfield = document.getElementById('typingspot');
 const timer = document.getElementById('timer');
-var starttimervalue = 60;
+const quotespot = document.getElementById('quote');
+var starttimervalue = 10;
+var score = 0; 
+var currentword = " ";
 
 const words = ["help", "socket", "snake", "python", "apple", "hello", "what", "they", "exquisite"];
-
 console.log("alive");
 
 const textfield = () => {
-    console.log("true");
     const textfieldvalue = inputfield.value.toLowerCase();
+    console.log("true");
     console.log(textfieldvalue);
     if (textfieldvalue === "start") {
         start();
         console.log("gamehasstarted");
+        inputfield.removeEventListener('input', textfield);
+        inputfield.addEventListener('input', main);
+        inputfield.value = "";
     }
     else {
         console.log("start hasn't been typed yet");
+    }
+}
+
+const wordgeneration = () => {
+    var randomword = words[Math.floor(Math.random()*words.length)];
+    return randomword;
+}
+
+currentword = wordgeneration();
+
+const main = () => {
+    quotespot.innerHTML = currentword;
+    const usersword = inputfield.value.toLowerCase();
+    if (usersword === currentword) {
+        currentword = wordgeneration();
+        quotespot.innerHTML = currentword;
+        score++;
+        console.log("nice");
+        inputfield.value = "";
+        startimervalue++;
     }
 }
 
@@ -39,5 +64,9 @@ function starttimer() {
  }
 
  function start() {
+    main();
     starttimer();
+    if (starttimervalue === 0) {
+        end();
+    }
  }
